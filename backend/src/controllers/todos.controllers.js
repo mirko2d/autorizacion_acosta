c61603725ad2817013e4837e6a51e2c9bdf3b204
mirko2de <mirkoacosta55@gmail.com>
2024-09-13 15:09:18 -0300

    cada usuario puede ver unicamente sus tareas

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -3,7 +3,7 @@ import { database } from "../db/database.js";
 export const getAllTodosCtrl = (req, res) => {
   const userId = req.user.id;
 
-  const userTodos = database.filter((todo) => todo.owner === userId)
+  const userTodos = database.todos.filter((todo) => todo.owner === userId)
 
   res.json({ todos: userTodos });
 };
